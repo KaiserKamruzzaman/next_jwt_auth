d7ffb0f11903fd35933831c787d4e1d935a34ded
Kaiser Kamruzzaman <38045495+KaiserKamruzzaman@users.noreply.github.com>
2022-05-03 23:44:32 +0200

    aunthtication process on going

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,5 +1,6 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 import { verify } from "jsonwebtoken";
+const secret = process.env.SECRET_TOKEN;
 
 export function middleware(req, ev) {
   console.log("middleware called...");
@@ -7,16 +8,55 @@ export function middleware(req, ev) {
 
   const jwt = cookies.MyJwtCookie;
   const url = req.nextUrl.clone();
-  console.log(url);
-
-  if (url.pathname.includes("/demo")) {
-    url.pathname = "/";
-    console.log("demo found...");
-    console.log(jwt);
-    if (jwt === undefined) {
-      console.log("undefine...");
-      return NextResponse.redirect(url);
+  //console.log(url);
+  if (url.pathname.includes("/")) {
+    if (!jwt) {
+      return NextResponse.next();
     }
   }
-  return NextResponse.next();
+
+  //   try {
+  //     let decode = verify(jwt, secret);
+  //   } catch (e) {
+  //     console.log("error paise...");
+  //   }
+
+  verify(jwt, secret, (err) => {
+    if (err) {
+      console.log("error found...");
+    } else {
+      console.log("no errro found...");
+    }
+  });
+
+  //   const token = verify(jwt, secret);
+  //   console.log(token);
+  //   if (token) {
+  //     console.log("token ase...");
+  //     return NextResponse.next();
+  //   }
+  //   if (token === "undefined") {
+  //     url.pathname = "/";
+  //     return NextResponse.redirect(url);
+  //   }
+  //   try {
+  //     console.log("middleware  found...");
+  //     const token = verify(jwt, secret);
+  //     url.pathname = "/dashboard";
+  //     return NextResponse.redirect(url);
+  //   } catch (e) {
+  //     console.log("middleware not found...");
+  //     url.pathname = "/";
+  //     return NextResponse.redirect(url);
+  //   }
+
+  //   if (url.pathname.includes("/demo")) {
+  //     url.pathname = "/";
+  //     console.log("demo found...");
+  //     console.log(jwt);
+  //     if (jwt === undefined) {
+  //       console.log("undefine...");
+  //       return NextResponse.redirect(url);
+  //     }
+  //   }
 }
