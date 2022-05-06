229d99af00d3b71b59770c0ae01260a7ec62d116
Kaiser Kamruzzaman <38045495+KaiserKamruzzaman@users.noreply.github.com>
2022-05-06 12:41:33 +0200

    add jwt-jwt auth process on going

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,34 +1,31 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { verify } from "jsonwebtoken";
-const secret = process.env.SECRET_TOKEN;
-
+import * as jose from "jose";
 export function middleware(req, ev) {
   console.log("middleware called...");
   const { cookies } = req;
-
   const jwt = cookies.MyJwtCookie;
   const url = req.nextUrl.clone();
-  //console.log(url);
-  if (url.pathname.includes("/")) {
-    if (!jwt) {
-      return NextResponse.next();
-    }
+  console.log(jwt);
+  // if (!jwt && url.pathname.includes("/")) {
+  //   return NextResponse.next();
+  // }
+  if (!jwt && url.pathname.includes("/dashboard")) {
+    console.log("paynai..");
+    url.pathname = "/";
+    return NextResponse.redirect(url);
   }
-
   //   try {
   //     let decode = verify(jwt, secret);
   //   } catch (e) {
   //     console.log("error paise...");
   //   }
-
-  verify(jwt, secret, (err) => {
-    if (err) {
-      console.log("error found...");
-    } else {
-      console.log("no errro found...");
-    }
-  });
-
+  // verify(jwt, secret, (err) => {
+  //   if (err) {
+  //     console.log("error found...");
+  //   } else {
+  //     console.log("no errro found...");
+  //   }
+  // });
   //   const token = verify(jwt, secret);
   //   console.log(token);
   //   if (token) {
@@ -49,7 +46,6 @@ export function middleware(req, ev) {
   //     url.pathname = "/";
   //     return NextResponse.redirect(url);
   //   }
-
   //   if (url.pathname.includes("/demo")) {
   //     url.pathname = "/";
   //     console.log("demo found...");
